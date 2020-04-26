ef7ee94a45555cc90d8a1bc21a04ec57409bf726
Nicolas <Nicolas@NicoCastro>
2020-04-26 18:39:55 -0300

    En la clase matriz se modificaron los bucles simples para ingresar los valores, por dos bucles anidados

--- src/tp8/Matriz.java
+++ src/tp8/Matriz.java
@@ -9,18 +9,18 @@ public class Matriz {
         matriz[1]=new int[3];
         matriz[2]=new int[5];
         matriz[3]=new int[2];
-        int i;
-        for(i=0;i<4;i++){
-            matriz[0][i]=(i+1)*3;
-        }
-        for(i=0;i<3;i++){
-            matriz[1][i]=15+i*3;
-        }
-        for(i=0;i<5;i++){
-            matriz[2][i]=24+i*3;
-        }
-        for(i=0;i<2;i++){
-            matriz[3][i]=39+i*3;
+        int i=0,j=0,k=1;
+        for(i=0;i<matriz.length;i++){
+            System.out.print("[ ");
+            for(j=0;j<matriz[i].length;j++){
+                matriz[i][j]=3*k;
+                k++;
+                System.out.print(matriz[i][j]);
+                if(j<(matriz[i].length-1)){
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("]");
         }
     }
 }
